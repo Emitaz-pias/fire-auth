1a444622a4c03ce4473cb815924ab65c04c79c1d
Emitaz-pias <61836021+Emitaz-pias@users.noreply.github.com>
2021-06-07 11:46:01 +0600

    sign and sign out method added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from "./firebase config";
+import { useState } from "react";
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  const [user, setUser] = useState({
+    isSignedIn: false,
+    name: "",
+    photo: "",
+    email: "",
+  });
+  const handleSignOut = ()=>{
+    firebase.auth().signOut()
+    .then((res)=>{
+      const signedOutUser = {
+        isSignedIn:false,
+        
+      }
+      setUser(signedOutUser)
+    })
+    .catch((err)=>{console.log(err)})
+  }
+  const handleSignIn = () => {
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then((result) => {
+        const { displayName, photoURL, email } = result.user;
+        const signedInUser = {
+          isSignedIn: true,
+          name: displayName,
+          photoURL: photoURL,
+          email: email,
+        };
+        setUser(signedInUser);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {user.isSignedIn ? (
+        <button onClick={handleSignOut}>Sign Out</button>
+      ) : (
+        <button onClick={handleSignIn}>Sign In</button>
+      )}
+      {user.isSignedIn && (
+        <div>
+          {" "}
+          <h2> welcome dear,{user.name}</h2>
+          <img src={user.photoURL} alt="" />
+          <h5>{user.email}</h5>
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/firebase config.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDqPj_d8FDGeGgpXAh_FvUuQRsxgKMm2dk",
+    authDomain: "ema-jhon-simple-aa344.firebaseapp.com",
+    projectId: "ema-jhon-simple-aa344",
+    storageBucket: "ema-jhon-simple-aa344.appspot.com",
+    messagingSenderId: "832356386810",
+    appId: "1:832356386810:web:0cb0aeff6894277977c643",
+    measurementId: "G-YL8RGY5EF6"
+  };
+  export default firebaseConfig
